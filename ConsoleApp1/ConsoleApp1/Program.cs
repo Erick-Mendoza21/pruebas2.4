24c9526dea0f9bb63689611665e90297ecc9109a
Hector Cruz <cch201300@gmail.com>
2023-09-08 10:50:54 -0600

    mensaje de error

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,12 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, Hector");
 
+
+Console.WriteLine("mensaje de error de git");
+
 Console.WriteLine("Mensaje 3");
+
+
+
 
 Console.WriteLine("vamos por unas chelasss");
